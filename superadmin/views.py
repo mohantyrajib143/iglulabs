a6a1e155c196172f700b3cee0e0e2e0e64ab7afd
Rajib <rajib@iglulabs.com>
2022-09-07 23:12:50 +0530

    1.Added dashboard.html and header.html 2.Added dashboard urls in urls.py and views.py

--- superadmin/views.py
+++ superadmin/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'superadmin/index.html')
+
+def dashboard(request):
+    return render(request,'superadmin/dashboard.html')
