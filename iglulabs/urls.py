cd1d2705d95269e01cbe66072b84c9fde328c2e4
Rajib <rajib@iglulabs.com>
2023-11-09 09:39:25 +0530

    added static file with templates : header.html and dashboard.html

--- iglulabs/urls.py
+++ iglulabs/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('superadmin.urls')),
     path('superadmin/', include('superadmin.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- superadmin/urls.py
+++ superadmin/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from superadmin import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('', views.login, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
