abb07174c96f65c64b7c4d1378221f6f890e9f4e
Rajib <rajib@iglulabs.com>
2023-11-09 12:05:46 +0530

    added project template

--- superadmin/urls.py
+++ superadmin/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from superadmin import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', views.mylogin, name='mylogin'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('projects/', views.projects, name='projects'),
 ]
